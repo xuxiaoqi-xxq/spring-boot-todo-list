86f10bdd62b130135c7931c8ae1d3ef54ebe5429
xuxiaoqi-xxq <xxq_work@163.com>
2020-08-05 17:51:05 +0800

    [eva] feature: implement should_return_request_todo_when_from_request_given_todo

--- src/main/java/com/oocl/todo/mapper/RequestTodoMapper.java
+++ src/main/java/com/oocl/todo/mapper/RequestTodoMapper.java
@@ -2,11 +2,14 @@ package com.oocl.todo.mapper;
 
 import com.oocl.todo.dto.TodoRequest;
 import com.oocl.todo.model.Todo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RequestTodoMapper {
     public TodoRequest from(Todo todo) {
-        return null;
+        TodoRequest todoRequest = new TodoRequest();
+        BeanUtils.copyProperties(todo, todoRequest);
+        return todoRequest;
     }
 }
