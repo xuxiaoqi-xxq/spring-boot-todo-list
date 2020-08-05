3e6b6fadbc82dea0de0351b632c6378917222bd4
carrymaniac <838537276@qq.com>
2020-08-05 16:27:51 +0800

    Initial commit

---
+++ src/test/java/com/oocl/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oocl.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
